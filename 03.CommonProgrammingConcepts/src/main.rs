d58e510c7e438ef3437c418da1207320b389f6f0
Zolo <zolo@unicornab.se>
2022-06-23 15:28:15 +0200

    array type example of &str

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+                            "August", "September", "October", "November", "December"];
 }
