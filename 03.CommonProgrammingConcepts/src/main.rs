913350faf4a131bea2cbe2a0dff95f0369875124
Zolo <zolo@unicornab.se>
2022-06-23 16:04:38 +0200

    inline comment

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,8 +1,4 @@
 fn main() {
-
-    // So we're doing somethin complicated here, long enough that we need
-    // multiple lines of comments to do it! Whew! Hopefully, this comment will
-    // explain what's going on.
-
+    let lucky_number = 7; // I'm feeling lucky today
 }
 
