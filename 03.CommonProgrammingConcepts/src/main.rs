81c70a5fb4bce2094f7998a208e81921a4d4681f
Zolo <zolo@unicornab.se>
2022-06-23 16:23:32 +0200

    iterating over a collection using for-in loop

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -5,10 +5,8 @@ fn main() {
     let a = [10, 20, 30, 40, 50];
     let mut index = 0;
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-        
-        index = index + 1;
+    for element in a.iter() {
+        println!("the value is: {}", element)
     }
 
     // the value is: 10
