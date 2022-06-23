6d8442df03db17e164e1f44fa72f20584f645838
Zolo <zolo@unicornab.se>
2022-06-23 14:51:11 +0200

    variable w/o mut

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    // x = 6; // <-- Error, cannot assign twice to immutable variable, x need mut
+    println!("The value of x is: {}", x)
 }
