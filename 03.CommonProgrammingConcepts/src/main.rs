66c8ed707d6605deba8518a74e4cf3bc8ca8a1e7
Zolo <zolo@unicornab.se>
2022-06-23 15:44:26 +0200

    array index out of bounds

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,7 +1,21 @@
+use std::io;
+
 fn main() {
     
     let a = [1,2,3,4,5];
 
-    // Accessing array elements
-    let first = a[0]; // --> value = 1
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index)
+        .expect("Failed to read line");
+        
+    let index: usize = index.trim()
+        .parse()
+        .expect("Not a number");
+        
+    let element = a[index];
+
+    println!("The value of the element at index {} is: {}", index, element)
 }
