5114a2ab057e95d502f3b53308b7b9a443fc5f9c
Zolo <zolo@unicornab.se>
2022-06-23 15:01:47 +0200

    error mismatched types example

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let spaces = "   ";
-    let spaces = spaces.len();
-    
-    println!("Lenght of spaces: {}", spaces);
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // <--- not legal, Error, expected `&str`, found `usize`
+
+    // println!("Lenght of spaces: {}", spaces);
 }
