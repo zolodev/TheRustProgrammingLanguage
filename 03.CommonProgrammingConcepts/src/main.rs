c656bcbc02449e5fc21d2e99f3ce22774a02cefd
Zolo <zolo@unicornab.se>
2022-06-23 15:45:32 +0200

    function example

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,21 +1,12 @@
 use std::io;
 
 fn main() {
-    
-    let a = [1,2,3,4,5];
 
-    println!("Please enter an array index.");
+    println!("Hello, world!");
 
-    let mut index = String::new();
+    another_function();
+}
 
-    io::stdin().read_line(&mut index)
-        .expect("Failed to read line");
-        
-    let index: usize = index.trim()
-        .parse()
-        .expect("Not a number");
-        
-    let element = a[index];
-
-    println!("The value of the element at index {} is: {}", index, element)
+fn another_function() {
+    println!("Another function");
 }
