9c822232c2b75429bb3e716410878398040e5a2d
Zolo <zolo@unicornab.se>
2022-06-23 16:03:51 +0200

    multiple lines of comments

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
 
-    // hello, world
+    // So we're doing somethin complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what's going on.
+
 }
 
