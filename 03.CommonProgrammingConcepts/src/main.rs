16dd6de1a5d9bdf07453ce8190cc9ee27b8ae4db
Zolo <zolo@unicornab.se>
2022-06-23 16:15:39 +0200

    repetition with loops, example of forever loop

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,13 +1,8 @@
 use core::num;
 
 fn main() {
-    let condition = true;
-    let number = if condition {
-        5
-    } else {
-        6
-    };
-
-    println!("The value of number is: {}", number); // The value of number is: 5
+    loop {
+        println!("again!");
+    }
 }
 
