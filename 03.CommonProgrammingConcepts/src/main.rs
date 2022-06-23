63e88b311f3bb75da68a4212842eb7bda868b01e
Zolo <zolo@unicornab.se>
2022-06-23 15:51:15 +0200

    variable declaration

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,13 +1,5 @@
-use std::io;
-
 fn main() {
 
-    println!("Hello, world!");
-
-    another_function(5, 6);
-}
-
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);   // The value of x is: 5
-    println!("The value of y is: {}", y);   // The value of x is: 6
+    // variable declaration
+    let y = 6;
 }
