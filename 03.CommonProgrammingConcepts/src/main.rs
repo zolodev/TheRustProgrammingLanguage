2c178af9738a39b554cfe45a8ce792f76555366f
Zolo <zolo@unicornab.se>
2022-06-23 15:47:59 +0200

    functions multiple params

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -4,9 +4,10 @@ fn main() {
 
     println!("Hello, world!");
 
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) {
+fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);   // The value of x is: 5
+    println!("The value of y is: {}", y);   // The value of x is: 6
 }
