5ccd2fe905f370439a669a38c84782fd3f6afa34
Zolo <zolo@unicornab.se>
2022-06-23 15:07:12 +0200

    operators

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
 
-    // Floating points represented according to IEEE-754 standard
-    let x = 2.0;    // f64
-    let y: f32 = 2.0;    // f32
+    // addition
+    let sum = 5 + 10;
 
-    println!("numbers: {} and {}", x, y);
+    // substraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let remainder = 43 % 5;
 }
