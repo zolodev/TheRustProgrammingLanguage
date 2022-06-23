025b8e7bc703b9b06e4999710b00995db7c13c01
Zolo <zolo@unicornab.se>
2022-06-23 16:00:31 +0200

    function param and return value

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,9 +1,9 @@
-fn five() -> i32 {
-    5
-}
-
 fn main() {
 
-    let x = five();
-    println!("The value of x is: {}", x);  // The value of x is: 5
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);  // The value of x is: 6
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
