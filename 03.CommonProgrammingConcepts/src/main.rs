ddbae8b3f0cfd24f11b3a2139c6dcc61e08d8454
Zolo <zolo@unicornab.se>
2022-06-23 15:21:32 +0200

    tuple destructure

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
 
-    let tup: (i32, f64, u8) = (500,6.4,1);
-    
+    let tup = (500,6.4,1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);   // The value of y is: 6.4
+
         
 }
