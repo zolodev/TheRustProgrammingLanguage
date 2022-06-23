d95681a2a163612ca1110dd7b61adae8cfb11bad
Zolo <zolo@unicornab.se>
2022-06-23 16:13:09 +0200

    inline expression

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,16 +1,13 @@
 use core::num;
 
 fn main() {
-    let number = 6;
-
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
+    let condition = true;
+    let number = if condition {
+        5
     } else {
-        println!("number is not divisible by 4, 3 or 2");
-    }
+        6
+    };
+
+    println!("The value of number is: {}", number); // The value of number is: 5
 }
 
