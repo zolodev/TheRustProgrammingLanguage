dbafa386b5a5270de4c458cefb247e7b431fa9b4
Zolo <zolo@unicornab.se>
2022-06-23 14:56:33 +0200

    shadowing variables

--- 03.CommonProgrammingConcepts/src/main.rs
+++ 03.CommonProgrammingConcepts/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
+    let x = 5;
+    
+    // Shadowing variable x
+    let x = x + 1;
+    
+    let x = x * 2;
 
-    x = 6;
-    println!("The value of x is: {}", x)
+    println!("The value of x is: {}", x);
 }
