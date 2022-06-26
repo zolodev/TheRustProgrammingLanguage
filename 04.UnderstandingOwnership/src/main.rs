4f817012a138dff9d2995bb0c8d8435ece88b3c4
Zolo <zolo@unicornab.se>
2022-06-26 22:40:19 +0200

    fixing the dangle refernce

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let reference_to_nothing = no_dangle();
 }
 
-// Error, expected named lifetime parameter
-// dangle returns a reference to a String
-fn dangle() -> &String {
-    let s = String::from("hello"); // s is anew String
+fn no_dangle() -> String {
+    let s = String::from("hello");
 
-    &s // we return a reference to the String, s
-} // Here, s goes out of scope, and is dropped. Its memory goes away.
-  // Danger!
+    s
+}
