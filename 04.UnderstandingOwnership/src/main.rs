b9a6a8fd5293c19207fee9d58ffcdbfb60f7a77e
Zolo <zolo@unicornab.se>
2022-06-26 21:30:07 +0200

    deep copy example

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -3,11 +3,12 @@ fn main() {
     let x = 5; // assign x with the value of 5
     let y = x; // copy the value from x to y, now y also have a value of 5
 
-    println!("x:{}, y:{}", x, y);
+    println!("x:{}, y:{}", x, y); // -> x:5, y:5
 
     // No let's look at the String version
     let s1 = String::from("hello");
-    let s2 = s1;
+    let s2 = s1.clone(); // Clone() is similare to deep copy
 
-    // println!("s1:{}, s2:{}", s1, s2); // Error, s1 -> value borrowed here after move
+    // No error s2 is now a copy of s1.
+    println!("s1:{}, s2:{}", s1, s2); // -> s1:hello, s2:hello
 }
