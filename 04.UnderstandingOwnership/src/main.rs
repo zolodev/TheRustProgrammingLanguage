63a9d43f53fd4bf8e1d6de1a2866de5d91f4adbd
Zolo <zolo@unicornab.se>
2022-06-26 21:59:39 +0200

    reference explained

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -7,5 +7,7 @@ fn main() {
 }
 
 fn calculate_length(s: &String) -> usize {
+    // s is a reference to a String
     s.len()
-}
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
