f834ec1f89f9a36fe5ce4e33c411b6f7eb286561
Zolo <zolo@unicornab.se>
2022-06-26 22:19:36 +0200

    immutable and mutable conflict

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-    } // r1 goes out of scope here, so we can make a new reference with no problems.
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM // -> Error, mutable borrow occurs here
 
-    let r2 = &mut s;
-
-    println!("{}", r2);
+    println!("{}, {}, {}", r1, r2, r3);
 }
