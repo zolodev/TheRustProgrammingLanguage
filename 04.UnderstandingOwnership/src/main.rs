c23bfcec94e78a2829438f468af92ddbf85f3b4f
Zolo <zolo@unicornab.se>
2022-06-26 21:41:13 +0200

    Re-using variables example

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -3,14 +3,18 @@ fn main() {
 
     takes_ownership(s);
 
+    // println!("{}", s); // Error, value borrowed here after move
+
     let x = 5;
-    makes_copy(x);
+    makes_copy(x); // -> 5
+
+    println!("{}", x);
 }
 
 fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+    println!("{}", some_string); // -> hello
 }
 
 fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+    println!("{}", some_integer); // -> 5
 }
