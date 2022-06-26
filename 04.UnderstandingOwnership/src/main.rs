b39fa8fafc36f155794b5147b7f6a6baad92127d
Zolo <zolo@unicornab.se>
2022-06-26 22:16:34 +0200

    How to resolve the need to race for the ref

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let mut s = String::from("hello");
 
-    let r1 = &mut s;
-    let r2 = &mut s; // Error, second mutable borrow occurs here
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
 
-    println!("{}, {}", r1, r2);
+    let r2 = &mut s;
+
+    println!("{}", r2);
 }
