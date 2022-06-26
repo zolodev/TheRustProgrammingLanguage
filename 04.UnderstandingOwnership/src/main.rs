a26a1e1b6e9b4728964b1373535b80cdd17f34c4
Zolo <zolo@unicornab.se>
2022-06-26 23:21:31 +0200

    differences explained

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,9 +1,25 @@
 use std::io::Bytes;
 
 fn main() {
-    let s = "Hello world!";
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "Hello world!";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 
     println!("{}", word); // -> Hello
 }
