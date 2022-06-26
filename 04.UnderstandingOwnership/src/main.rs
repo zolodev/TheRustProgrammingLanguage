bc053145833a7505b71b80649069bfcbf4363e14
Zolo <zolo@unicornab.se>
2022-06-26 22:57:38 +0200

    slicing a String

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,24 +1,8 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+    let hello = &s[0..5];
+    let world = &s[6..11];
 
-    s.clear(); // this empties the String, makit it equal to ""
-
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
-
-    println!("{}", word); // -> 5
-}
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
+    println!("{} {}", hello, world); // -> hello world
 }
