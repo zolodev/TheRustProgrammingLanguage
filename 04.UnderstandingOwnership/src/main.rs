6ede7f37b253d3153934b0e0e5c975ea1e3a5f57
Zolo <zolo@unicornab.se>
2022-06-26 23:24:04 +0200

    slicing array with i32

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,37 +1,9 @@
 use std::io::Bytes;
 
 fn main() {
-    let my_string = String::from("hello world");
+    let a = [1, 2, 3, 4, 5];
 
-    // `first_word` works on slices of `String`s, whether partial or whole
-    let word = first_word(&my_string[0..6]);
-    let word = first_word(&my_string[..]);
+    let slice = &a[1..3];
 
-    // `first_word` also works on references to `String`s, which are equivalent
-    // to whole slices of `String`s
-    let word = first_word(&my_string);
-
-    let my_string_literal = "Hello world!";
-
-    // `first_word` works on slices of string literals, whether partial or whole
-    let word = first_word(&my_string_literal[0..6]);
-    let word = first_word(&my_string_literal[..]);
-
-    // Because string literals *are* string slices already,
-    // this works too, without the slice syntax!
-    let word = first_word(my_string_literal);
-
-    println!("{}", word); // -> Hello
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
+    assert_eq!(slice, &[2, 3]);
 }
