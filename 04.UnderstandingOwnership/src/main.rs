654b7a3134d0d1b578047c35dec1a8c7e276e49a
Zolo <zolo@unicornab.se>
2022-06-26 23:02:44 +0200

    optimized slice and more examples

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     let s = String::from("hello world");
 
-    let hello = &s[0..5];
-    let world = &s[6..11];
+    // let len = s.len();
 
-    println!("{} {}", hello, world); // -> hello world
+    // let slice = &s[0..2];
+    // let slice = &s[..2]; // the same without the zero
+
+    // let slice = &s[3..len];
+    // let slice = &s[3..]; // the same without len
+
+    println!("{} {}", &s[..5], &s[6..]); // -> hello world
 }
