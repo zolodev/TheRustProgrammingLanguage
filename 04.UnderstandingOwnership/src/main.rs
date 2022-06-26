9e4b13a79d7f402e9a5e55b53590416a532223cd
Zolo <zolo@unicornab.se>
2022-06-26 21:56:08 +0200

    optimized to use reference

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{}' is '{}'", s2, len); // -> The length of 'hello' is '5'
+    println!("The length of '{}' is '{}'", s1, len); // -> The length of 'hello' is '5'
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
