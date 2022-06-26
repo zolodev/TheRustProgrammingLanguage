3db9e17f02f31c74badfad454a7413b4b8c015be
Zolo <zolo@unicornab.se>
2022-06-26 21:38:45 +0200

    Functions with ownership and scope annotated

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
-    // Values copied to stack (not heap)
-    let x = 5; // assign x with the value of 5
-    let y = x; // copy the value from x to y, now y also have a value of 5
+    let s = String::from("hello");
 
-    println!("x:{}, y:{}", x, y); // -> x:5, y:5
+    takes_ownership(s);
 
-    // No let's look at the String version
-    let s1 = String::from("hello");
-    let s2 = s1.clone(); // Clone() is similare to deep copy
+    let x = 5;
+    makes_copy(x);
+}
 
-    // No error s2 is now a copy of s1.
-    println!("s1:{}, s2:{}", s1, s2); // -> s1:hello, s2:hello
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
