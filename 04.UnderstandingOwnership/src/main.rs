f2a2d8f7464729917b13cdef7be46a809454cc99
Zolo <zolo@unicornab.se>
2022-06-26 22:33:33 +0200

    Dangling references is not allowed

--- 04.UnderstandingOwnership/src/main.rs
+++ 04.UnderstandingOwnership/src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
-    let mut s = String::from("hello");
+    let reference_to_nothing = dangle();
+}
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{}, {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
+// Error, expected named lifetime parameter
+fn dangle() -> &String {
+    let s = String::from("hello");
 
-    let r3 = &mut s; // no problem
-
-    println!("{}", r3);
+    &s
 }
