d5068e089f262c963f9f0b0983484eeff49dc2f9
Zolo <zolo@unicornab.se>
2022-06-27 15:07:44 +0200

    Tuple structs w/o named fields

--- 05.UsingStructsToStructureRelatedData/src/main.rs
+++ 05.UsingStructsToStructureRelatedData/src/main.rs
@@ -1,50 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
+// Using a Tuple struct without Named Fields
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 fn main() {
-    let mut user1 = User {
-        email: String::from("ex@example.com"),
-        username: String::from("username123"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    println!(
-        "active: {}, email: {}, username: {}, sign in no. {}",
-        user1.active, user1.email, user1.username, user1.sign_in_count
-    ); // -> active: true, email: ex@example.com, username: username123, sign in no. 1
-
-    user1.email = String::from("newemail@example.com");
-
-    println!("new email: {}", user1.email); // -> new email: newemail@example.com
-
-    let user2 = build_user(String::from("usr2@example.com"), String::from("user2"));
-
-    println!(
-        "user2data: user: {}, email: {}",
-        user2.username, user2.email
-    ); // -> user2data: user: user2, email: usr2@example.com
-
-    let user3 = User {
-        email: String::from("another@example.com"),
-        ..user2 // Short hand copy data from one struct to another, similar to javascript spread
-    };
-
-    println!(
-        "user3data: user: {}, email: {}",
-        user3.username, user3.email
-    ); // -> user3data: user: user2, email: another@example.com
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,    // shorthand field populate, due to the field is the same name as the parameter
-        username, // shorthand field populate, due to the field is the same name as the parameter
-        active: true,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
