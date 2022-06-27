589e81930cfa520858d03f95e288c4ef5048a773
Zolo <zolo@unicornab.se>
2022-06-27 14:26:49 +0200

    init struct example

--- 05.UsingStructsToStructureRelatedData/src/main.rs
+++ 05.UsingStructsToStructureRelatedData/src/main.rs
@@ -1,3 +1,20 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("ex@example.com"),
+        username: String::from("username123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!(
+        "active: {}, email: {}, username: {}, sign in no. {}",
+        user1.active, user1.email, user1.username, user1.sign_in_count
+    ); // -> active: true, email: ex@example.com, username: username123, sign in no. 1
 }
