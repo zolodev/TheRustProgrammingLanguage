5ae252c1ae2a49e2508fb021c81e112426076fff
Zolo <zolo@unicornab.se>
2022-06-28 10:52:12 +0200

    implementation of a struct

--- 05.UsingStructsToStructureRelatedData/src/main.rs
+++ 05.UsingStructsToStructureRelatedData/src/main.rs
@@ -3,12 +3,21 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    ); // -> The area of the rectangle is 1500 square pixels.
 }
