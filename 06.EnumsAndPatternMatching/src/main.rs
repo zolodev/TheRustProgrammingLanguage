f1c983bd7076a2f8b1cb05bcf934a7f82ea62cce
Zolo <zolo@unicornab.se>
2022-06-29 10:26:40 +0200

    no struct only enum

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,21 +1,10 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
-//It is possible to combine structs and enums
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // No need for a struct, we can put data directly into each enum variant
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
