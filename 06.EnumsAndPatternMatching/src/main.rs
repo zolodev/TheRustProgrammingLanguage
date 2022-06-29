6c2b58a93eac89f1e084d4a04467aadb17f49c40
Zolo <zolo@unicornab.se>
2022-06-29 10:30:13 +0200

    enums assoiciated multiple data

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,10 +1,9 @@
 enum IpAddrKind {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
-    // No need for a struct, we can put data directly into each enum variant
-    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let home = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
 }
