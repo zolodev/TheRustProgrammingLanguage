724621982d5b9bb06092a317ff875615533bc5d1
Zolo <zolo@unicornab.se>
2022-06-29 10:20:51 +0200

    enum basics

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4,
+    V6,
 }
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    // Functions can take an enum as a parameter
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
+
+// Functions can take an enum as a parameter
+fn route(ip_kind: IpAddrKind) {}
