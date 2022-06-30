163e639c4e1feb546d2475700c68b781a687196f
Zolo <zolo@unicornab.se>
2022-06-30 06:50:38 +0200

    Binding values using enum with another enum

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,14 +1,23 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 fn main() {
     println!("How many cents? {}", value_in_cents(Coin::Penny)); // -> How many cents? 1
     println!("How many cents? {}", value_in_cents(Coin::Nickel)); // -> How many cents? 5
     println!("How many cents? {}", value_in_cents(Coin::Dime)); // -> How many cents? 10
-    println!("How many cents? {}", value_in_cents(Coin::Quarter)); // -> How many cents? 25
+    println!(
+        "How many cents? {}",
+        value_in_cents(Coin::Quarter(UsState::Alabama))
+    ); // -> How many cents? 25
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -19,6 +28,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state); // -> State quarter from Alabama
+            25
+        }
     }
 }
