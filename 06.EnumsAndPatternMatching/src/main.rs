6a5fb93de1fe7525b4f4324eb46c9b0a20cd103e
Zolo <zolo@unicornab.se>
2022-06-30 07:32:17 +0200

    if let shorter syntax example

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     let config_max = Some(3u8);
 
     // match will only execute when config_max is Some()
-    match config_max {
-        Some(max) => println!("The maximum is configured to be {}", max),
-        _ => (),
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
+
+    // same code as above, with if-let and shorter
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
     }
 }
