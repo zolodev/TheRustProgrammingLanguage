e894212a73424b5c4a89f3ac5447d6abeba4735d
Zolo <zolo@unicornab.se>
2022-06-30 07:15:46 +0200

    Catch-all arm example

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-
-    println!("Result {:?}", six); // -> Result Some(6)
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
+    // Not a Random value,
+    // because the focus of the example is about the match
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other), // other => Catch-all arm
     }
 }
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
