f15dee3926544c47bfe8990c445cc492892804d5
Zolo <zolo@unicornab.se>
2022-06-30 07:29:51 +0200

    match that runs only when Some()

--- 06.EnumsAndPatternMatching/src/main.rs
+++ 06.EnumsAndPatternMatching/src/main.rs
@@ -1,14 +1,9 @@
 fn main() {
-    // Not a Random value,
-    // because the focus of the example is about the match
-    let dice_roll = 9;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
-        // example of how to ignore all other values
-        _ => (), // '_' using a tuple type, will ignore all other values except 3 & 7 and do nothing
+    let config_max = Some(3u8);
+
+    // match will only execute when config_max is Some()
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
     }
 }
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
