a5e8746d9d58e87e5a7d152898088faffdb12870
Zolo <zolo@unicornab.se>
2022-07-04 10:09:26 +0200

    using super to access parent module function

--- 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
+++ 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
@@ -14,6 +14,17 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+fn deliver_order() {}
+
 pub fn eat_at_resturant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
