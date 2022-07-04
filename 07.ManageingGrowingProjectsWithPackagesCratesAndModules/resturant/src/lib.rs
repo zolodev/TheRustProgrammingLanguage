b2872fb5bc9028caa1d76a67a9b5b1da52e425f4
Zolo <zolo@unicornab.se>
2022-07-04 10:43:07 +0200

    looking at motr public and private keyword

--- 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
+++ 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
@@ -15,6 +15,21 @@ mod front_of_house {
 }
 
 mod back_of_house {
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
@@ -26,6 +41,18 @@ mod back_of_house {
 fn deliver_order() {}
 
 pub fn eat_at_resturant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about twhat bread we'd like
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
