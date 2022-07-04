73db53a37b71ef86fbf526c0a32f337e829a6185
Zolo <zolo@unicornab.se>
2022-07-04 13:51:02 +0200

    glob operator for all public items

--- 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/main.rs
+++ 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/main.rs
@@ -3,7 +3,11 @@
 // will make the list shorter
 use std::io::{self, Write};
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+
+// Glob Operator '*'
+// if we want to bring in _all_ public items
+use std::collections::*;
 
 fn main() {
     let mut map = HashMap::new();
