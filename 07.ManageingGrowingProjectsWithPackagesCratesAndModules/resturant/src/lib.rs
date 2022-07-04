e70f8d97993f81ddec30e0d0ce319b9e0bb0d1c3
Zolo <zolo@unicornab.se>
2022-07-04 11:32:44 +0200

    wrap in a module and adding super

--- 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
+++ 07.ManageingGrowingProjectsWithPackagesCratesAndModules/resturant/src/lib.rs
@@ -47,30 +47,32 @@ fn deliver_order() {}
 
 // Create a shortcut to hosting
 use crate::front_of_house::hosting;
+mod customer {
 
-pub fn eat_at_resturant() {
-    // All enums options are available when public
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    pub fn eat_at_resturant() {
+        // All enums options are available when public
+        let order1 = super::back_of_house::Appetizer::Soup;
+        let order2 = super::back_of_house::Appetizer::Salad;
 
-    // Order a breakfast in the summer with Rye toast
-    let mut meal = back_of_house::Breakfast::summer("Rye");
+        // Order a breakfast in the summer with Rye toast
+        let mut meal = super::back_of_house::Breakfast::summer("Rye");
 
-    // Change our mind about twhat bread we'd like
-    meal.toast = String::from("Wheat");
+        // Change our mind about twhat bread we'd like
+        meal.toast = String::from("Wheat");
 
-    println!("I'd like {} toast please", meal.toast);
+        println!("I'd like {} toast please", meal.toast);
 
-    // The next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal fruit that comes with the meal
-    // meal.seasonal_fruit = String::from("blueberries");
+        // The next line won't compile if we uncomment it; we're not allowed
+        // to see or modify the seasonal fruit that comes with the meal
+        // meal.seasonal_fruit = String::from("blueberries");
 
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+        // Absolute path
+        crate::front_of_house::hosting::add_to_waitlist();
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+        // Relative path
+        super::front_of_house::hosting::add_to_waitlist();
 
-    // Shortcut variant
-    hosting::add_to_waitlist();
+        // Shortcut variant
+        super::hosting::add_to_waitlist();
+    }
 }
