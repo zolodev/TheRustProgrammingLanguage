338eba7018281f5af2fd96a811f8f9b0220cf5c9
Zolo <zolo@unicornab.se>
2022-07-05 12:59:27 +0200

    getting values from a vector

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    {
-        let v = vec![1, 2, 3, 4];
+    let v = vec![1, 2, 3, 4];
 
-        // do stuff with v
-    } // <- v goes out of scope and is freed here
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
 }
