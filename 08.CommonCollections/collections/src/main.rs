eeab75a17515b634a63baeebc953a28b21ce3c8c
Zolo <zolo@unicornab.se>
2022-07-05 12:55:03 +0200

    freeing a vector

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    let mut v = Vec::new();
+    {
+        let v = vec![1, 2, 3, 4];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+        // do stuff with v
+    } // <- v goes out of scope and is freed here
 }
