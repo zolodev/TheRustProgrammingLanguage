70dd1e82ddab89300886aa16eb63a4922d3e8698
Zolo <zolo@unicornab.se>
2022-07-06 10:37:20 +0200

    iterate vector

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,24 +1,8 @@
 fn main() {
-    let v = vec![1, 2, 3, 4];
+    let v = vec![100, 32, 57];
 
-    let third: &i32 = &v[2];
-    println!("The third element is {}", third);
-
-    match v.get(2) {
-        Some(third) => println!("The third element is {}", third),
-        None => println!("There is no third element."),
+    // Iterate over a immutable vector
+    for i in &v {
+        println!("{}", i);
     }
-
-    // let does_not_exist = &v[100];   // Error, thread 'main' panicked at 'index out of bounds: the len is 4 but the index is 100', src\main.rs:12:27
-    let does_not_exist = v.get(100);
-
-    println!("does it exist? {:?}", does_not_exist); // -> does it exist? None
-
-    // This would be the better alternative to raw unwrap
-    if let Some(item) = does_not_exist {
-        println!("does it exist? {}", item); // Will never return while index is out of bounds
-    }
-
-    // Unwrapping can be dangerous, use with caution
-    // println!("does it exist? {}", does_not_exist.unwrap()); // Error, thread 'main' panicked at 'called `Option::unwrap()` on a `None` value', src\main.rs:22:50
 }
