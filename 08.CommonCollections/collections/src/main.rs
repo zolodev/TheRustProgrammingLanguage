e13cc9b490b70bb5a4efa0b3ca5d9a3001cbfb09
Zolo <zolo@unicornab.se>
2022-07-06 16:16:04 +0200

    Explaining why Strings are complicated

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,3 +1,14 @@
+/// ♥ To summarize, strings are complicated. Different programming
+/// languages make different choices about how to present this
+/// complexity to the programmer. Rust has chosen to make the
+/// correct handling of String data the default behavior for all
+/// Rust programs, which means programmers have to put more thought
+/// into handling UTF-8 data upfront. This trade-off exposes more
+/// of the complexity of strings than is apparent in other
+/// programming languages, but it prevents you from having to
+/// handle errors involving non-ASCII characters later in your
+/// development life cycle. ♥
+
 fn main() {
     let mut s = String::from("foo");
     s.push_str("bar");
