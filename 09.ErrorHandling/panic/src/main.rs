2830c61212e2ba5b94fd1497737869c5e74289ef
Zolo <zolo@unicornab.se>
2022-07-06 22:00:12 +0200

    Init Adding chapter 09

---
+++ 09.ErrorHandling/panic/src/main.rs
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Wed Jul 06 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 09-01
+ *                 Errors with Panic
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+fn main() {}
