ed15ce3b90be0eae21906c40fe6f61cf54a7df91
Zolo <zolo@unicornab.se>
2022-07-06 19:01:06 +0200

    Getting data from HashMap

--- 08.CommonCollections/hashmap/src/main.rs
+++ 08.CommonCollections/hashmap/src/main.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
 fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue team"), 10);
+    scores.insert(String::from("Red team"), 50);
+
     let field_name = String::from("Favorite color");
     let field_value = String::from("Blue");
 
@@ -9,4 +14,15 @@ fn main() {
 
     println!("{:?}", map); // -> {"Favorite color": "Blue"}
     println!("{:?}", map[&field_name]); // -> "Blue"
+
+    let team_name = String::from("Blue team");
+    let score = scores.get(&team_name);
+    println!("{}", score.unwrap()); // -> 10
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+        // Output:
+        // -> Blue team: 10
+        // -> Red team: 50
+    }
 }
