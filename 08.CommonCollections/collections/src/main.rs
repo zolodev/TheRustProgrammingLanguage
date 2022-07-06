175fb017ad5ddc691c892a644bca0911a05a54ad
Zolo <zolo@unicornab.se>
2022-07-06 12:21:34 +0200

    usage of to_string() on str literals

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,16 +1,7 @@
-#[derive(Debug)]
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
 fn main() {
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
+    let data = "initial contents";
+    let s = data.to_string();
 
-    // dbg!(row);
-    println!("{:?}", row); // -> [Int(3), Text("blue"), Float(10.12)]
+    // the moethod also works on a literal directly
+    let s = "initial contents".to_string();
 }
