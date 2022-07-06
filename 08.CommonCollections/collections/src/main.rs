f47b071c06c97d886863afe44c60a890f5aff842
Zolo <zolo@unicornab.se>
2022-07-06 10:39:27 +0200

    iterate and updating vector

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let v = vec![100, 32, 57];
+    let mut v = vec![100, 32, 57];
 
-    // Iterate over a immutable vector
+    // updating values in vector
+    for i in &mut v {
+        *i += 50
+    }
+
+    // Iterate over a mutable vector after it has changed
     for i in &v {
         println!("{}", i);
     }
