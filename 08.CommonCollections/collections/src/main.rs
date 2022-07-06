2d27589074e7682b82a1fd8fae608daeedd58603
Zolo <zolo@unicornab.se>
2022-07-06 12:25:55 +0200

    same same

--- 08.CommonCollections/collections/src/main.rs
+++ 08.CommonCollections/collections/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let data = "initial contents";
     let s = data.to_string();
 
-    // the moethod also works on a literal directly
+    // the method also works on a literal directly
     let s = "initial contents".to_string();
+    let s = String::from("initial contents"); // Same as above
 }
