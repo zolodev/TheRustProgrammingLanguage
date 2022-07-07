6f19d92ff85d817325bcf1669183959e40890a2f
Zolo <zolo@unicornab.se>
2022-07-07 16:21:42 +0200

    Alternative way using clojures

--- 09.ErrorHandling/error-handling/src/main.rs
+++ 09.ErrorHandling/error-handling/src/main.rs
@@ -12,16 +12,26 @@ use std::{fs::File, io::ErrorKind};
 fn main() {
     let f: Result<File, std::io::Error> = File::open("hello.txt");
 
-    let _f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file {:?}", e),
-            },
-            _other_error => {
-                panic!("Problem opening the file {:?}", _other_error)
-            }
-        },
-    };
+    // let _f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file {:?}", e),
+    //         },
+    //         _other_error => {
+    //             panic!("Problem opening the file {:?}", _other_error)
+    //         }
+    //     },
+    // };
+
+    // Alternative using closure
+    let _f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt")
+                .unwrap_or_else(|error| panic!("Problem creating the file: {:?}", error))
+        } else {
+            panic!("Problem opening the file: {:?}", error)
+        }
+    });
 }
