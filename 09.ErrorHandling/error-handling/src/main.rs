b9ce7cfaceca84cb88e8eac7a40e4abf85731ba1
Zolo <zolo@unicornab.se>
2022-07-07 17:07:47 +0200

    propagating errors

--- 09.ErrorHandling/error-handling/src/main.rs
+++ 09.ErrorHandling/error-handling/src/main.rs
@@ -7,7 +7,10 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 // use std::io::ErrorKind;
 
 fn main() {
@@ -36,5 +39,26 @@ fn main() {
     //     }
     // });
 
-    let _f = File::open("hello.txt").expect("Failed to open hello.txt");
+    // let _f = File::open("hello.txt").expect("Failed to open hello.txt");
+
+    let f = read_username_from_file();
+    if f.is_err() {
+        panic!("Can not read username from file");
+    }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
