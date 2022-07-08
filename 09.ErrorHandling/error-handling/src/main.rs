61b3c4e48a284ff650a17bf6968d56c67a9ea1db
Zolo <zolo@unicornab.se>
2022-07-08 09:46:32 +0200

    optimizing the function

--- 09.ErrorHandling/error-handling/src/main.rs
+++ 09.ErrorHandling/error-handling/src/main.rs
@@ -48,17 +48,9 @@ fn main() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
+    let mut f = File::open("hello.txt")?;
     let mut s = String::new();
 
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
