55fac42ceaa9aa491122a12e7bdfc47de02bae72
Zolo <zolo@unicornab.se>
2022-07-08 13:00:17 +0200

    Improving Guessing game with error handling

---
+++ 09.ErrorHandling/guessing-game/src/main.rs
@@ -0,0 +1,66 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Fri Jul 08 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Adding some error handling and a type from chapter 09-03
+ *                 to the guessing game made in chapter 02
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let guess = Guess::new(guess);
+
+        println!("You guessed: {}", guess.value);
+
+        match guess.value.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
