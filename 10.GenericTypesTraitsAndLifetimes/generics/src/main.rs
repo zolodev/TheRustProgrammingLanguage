8d23c644796d8e17a3fb19caa0750ab9e94d53a2
Zolo <zolo@unicornab.se>
2022-07-08 20:33:44 +0200

    duplicated code very bad

--- 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
@@ -19,4 +19,16 @@ fn main() {
     }
 
     println!("The largets number is {}", largets);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largets = number_list[0];
+
+    for number in number_list {
+        if number > largets {
+            largets = number
+        }
+    }
+
+    println!("The largets number is {}", largets);
 }
