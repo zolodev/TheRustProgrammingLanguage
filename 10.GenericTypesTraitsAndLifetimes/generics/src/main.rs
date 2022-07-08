8b9de184fbca4dcf1f8dc9f18c34ea205af44a38
Zolo <zolo@unicornab.se>
2022-07-08 21:15:52 +0200

    Problem to compile w/o cmp trait

--- 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
@@ -10,30 +10,20 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largets number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largets number is {}", result);
 }
 
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
-    for &number in list {
-        if number > largest {
-            largest = number
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-    for &number in list {
-        if number > largest {
-            largest = number
+    for &item in list {
+        if item > largest {
+            largest = item
         }
     }
     largest
