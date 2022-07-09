3a0fbe8c6e6fd251a294d82b5eb6863733aa0826
Zolo <zolo@unicornab.se>
2022-07-09 10:35:40 +0200

    mixing up the pointer as a return value

--- 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
@@ -8,20 +8,17 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 #[derive(Debug)]
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
-
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
@@ -36,10 +33,10 @@ fn main() {
     // let result = largest(&char_list);
     // println!("The largets number is {}", result);
 
-    // This can also be used with two of the same type
-    let p: Point<f32> = Point { x: 5.0, y: 1.0 };
-
-    println!("p.x = {}", p.distance_from_origin());
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
 
 // fn largest<T>(list: &[T]) -> T {
