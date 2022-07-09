ef5bb7d2ed71b124abe4c37554ab75e14d4edb98
Zolo <zolo@unicornab.se>
2022-07-09 08:52:03 +0200

    testing generics on struct

--- 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
@@ -7,24 +7,36 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
-    println!("The largets number is {}", result);
+    // let result = largest(&number_list);
+    // println!("The largets number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&char_list);
-    println!("The largets number is {}", result);
+    // let result = largest(&char_list);
+    // println!("The largets number is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("int point {:?}", integer);
+    println!("float point {:?}", float);
 }
 
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-    for &item in list {
-        if item > largest {
-            largest = item
-        }
-    }
-    largest
-}
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+// for &item in list {
+//     if item > largest {
+//         largest = item
+//     }
+// }
+//     largest
+// }
