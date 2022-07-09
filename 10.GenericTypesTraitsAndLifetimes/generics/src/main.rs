885472fea5eb77308a986198f4fca1657d51124c
Zolo <zolo@unicornab.se>
2022-07-09 09:43:59 +0200

    Another example of implementating a generic

--- 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/generics/src/main.rs
@@ -8,26 +8,38 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 #[derive(Debug)]
-struct Point<T, U> {
+struct Point<T> {
     x: T,
-    y: U, // Will fix the problem using different types
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    // let number_list = vec![34, 50, 25, 100, 65];
 
     // let result = largest(&number_list);
     // println!("The largets number is {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    // let char_list = vec!['y', 'm', 'a', 'q'];
 
     // let result = largest(&char_list);
     // println!("The largets number is {}", result);
 
     // This can also be used with two of the same type
-    let wont_work = Point { x: 5, y: 4 };
+    let p: Point<f32> = Point { x: 5.0, y: 1.0 };
 
-    println!("wont_work point {:?}", wont_work);
+    println!("p.x = {}", p.distance_from_origin());
 }
 
 // fn largest<T>(list: &[T]) -> T {
