2eff9431533a3d1042cb2d7f32b428237bde9a76
Zolo <zolo@unicornab.se>
2022-07-10 22:27:10 +0200

    lifetime makes it work between scopes

--- 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
@@ -8,11 +8,13 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string1 = String::from("long string is long");
+    {
+        let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
