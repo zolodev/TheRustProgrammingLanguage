238b14c1a16de226d9eaaf38de982a6d01b490fd
Zolo <zolo@unicornab.se>
2022-07-10 22:19:21 +0200

    fixing the problem using lifetimes

--- 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
@@ -8,11 +8,17 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 fn main() {
-    // Preventing Dangling References with Lifetimes
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    {
-        let x = 5;
-        let r = &x;
-        println!("r: {}", r);
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
