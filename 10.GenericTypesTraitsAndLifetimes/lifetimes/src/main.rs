31dfb4cb6d54cc8e505a7b8cca839f0ffef55c1d
Zolo <zolo@unicornab.se>
2022-07-11 15:36:48 +0200

    lifetime elision rules

--- 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
@@ -12,6 +12,19 @@ struct ImportantExcerpt<'a> {
     part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+    //  first elision rule, because &self is the only parameter
+    fn level(&self) -> i32 {
+        3
+    }
+
+    // first lifetime elision rule applies, because first parameter is a &self
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
 fn main() {
     let string1 = String::from("long string is long");
     {
@@ -22,12 +35,13 @@ fn main() {
     }
 
     let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
+    let first = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first };
 
     println!("Part: {}", i.part);
+
+    i.announce_and_return_part("announcement");
+    println!("Level: {}", i.level());
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
