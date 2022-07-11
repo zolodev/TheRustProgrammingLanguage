ee75c5aa9c7e3da9b3ab53a6f7ae209efbab6830
Zolo <zolo@unicornab.se>
2022-07-11 15:17:46 +0200

    struct lifetime

--- 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
@@ -7,6 +7,11 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+// Struct Lifetime
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("long string is long");
     {
@@ -15,6 +20,14 @@ fn main() {
         let result = longest(string1.as_str(), string2);
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("Part: {}", i.part);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
