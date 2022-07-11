a20f88855a1ebfc412b827ec804c0d3374268393
Zolo <zolo@unicornab.se>
2022-07-11 16:06:57 +0200

    All together and combined

--- 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
+++ 10.GenericTypesTraitsAndLifetimes/lifetimes/src/main.rs
@@ -58,6 +58,20 @@ impl<'a> ImportantExcerpt<'a> {
     }
 }
 
+use std::fmt::Display;
+
+fn longest_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("long string is long");
     {
@@ -78,6 +92,12 @@ fn main() {
 
     // Static lifetime
     let s: &'static str = "I have a static lifetime.";
+    println!("Static str: {}", s);
+
+    // Specifying generic type parameters, trait bounds and lifetimes together
+    let la = longest_announcement("long string", "xyz", "My Attention!");
+
+    println!("Longest announcement: {}", la);
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
