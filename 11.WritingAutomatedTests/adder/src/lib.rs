087356e476fb496997a916f095496c776daea1bb
Zolo <zolo@unicornab.se>
2022-07-12 08:12:28 +0200

    using Result as a return value for testing

--- 11.WritingAutomatedTests/adder/src/lib.rs
+++ 11.WritingAutomatedTests/adder/src/lib.rs
@@ -9,33 +9,12 @@
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_then_100() {
-        Guess::new(200);
-    }
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}",
-                value
-            );
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
         }
-
-        Guess { value }
     }
 }
