10be5f83cd3264df25a714ec433a3a654627a3b9
Zolo <zolo@unicornab.se>
2022-07-12 07:46:58 +0200

    testing strings

--- 11.WritingAutomatedTests/adder/src/lib.rs
+++ 11.WritingAutomatedTests/adder/src/lib.rs
@@ -12,11 +12,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"));
     }
 }
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
 }
