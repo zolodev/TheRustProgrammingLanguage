c125acd6de1f9a8c1b6f57cecec1c43f71a38000
Zolo <zolo@unicornab.se>
2022-07-12 07:50:10 +0200

    Adding a custom error message

--- 11.WritingAutomatedTests/adder/src/lib.rs
+++ 11.WritingAutomatedTests/adder/src/lib.rs
@@ -14,7 +14,11 @@ mod tests {
     #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
-        assert!(result.contains("Carol"));
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
 
