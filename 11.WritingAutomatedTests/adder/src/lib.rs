790aae9bc293c0ba063293bdc2ed8451d2376a4a
Zolo <zolo@unicornab.se>
2022-07-12 06:27:47 +0200

    adding header

--- 11.WritingAutomatedTests/adder/src/lib.rs
+++ 11.WritingAutomatedTests/adder/src/lib.rs
@@ -1,3 +1,12 @@
+/*****************************************************************************
+ * Filename      : lib.rs
+ * Created       : Tue Jul 12 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 11-01 writing tests
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
 #[cfg(test)]
 mod tests {
     #[test]
