daa3a3a055bb6624e3b79fdb7e45a50048735afb
Zolo <zolo@unicornab.se>
2022-07-13 08:15:00 +0200

    testing internal functions

--- 11.WritingAutomatedTests/adder/src/lib.rs
+++ 11.WritingAutomatedTests/adder/src/lib.rs
@@ -7,14 +7,20 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2))
     }
 }
