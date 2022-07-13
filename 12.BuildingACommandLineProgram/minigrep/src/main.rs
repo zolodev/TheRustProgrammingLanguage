65c41db485b960251c24a3e6b049b1745039438d
Zolo <zolo@unicornab.se>
2022-07-13 10:22:22 +0200

    init commit

---
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Wed Jul 13 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Building a Command Line Program minigrep
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {}
