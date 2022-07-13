f319218d0084c20152198c232796934683e18df6
Zolo <zolo@unicornab.se>
2022-07-13 08:48:20 +0200

    sharing code between integrations tests

--- 11.WritingAutomatedTests/adder/tests/integration_test.rs
+++ 11.WritingAutomatedTests/adder/tests/integration_test.rs
@@ -1,6 +1,12 @@
 use adder;
 
+// Include the module common for shared test code
+mod common;
+
 #[test]
 fn it_add_two() {
+    // Calling shared test code
+    common::setup();
+
     assert_eq!(4, adder::add_two(2))
 }
