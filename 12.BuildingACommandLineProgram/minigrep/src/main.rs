d2c39487a6177764c20c17517e19f6a5d3e81fad
Zolo <zolo@unicornab.se>
2022-07-13 10:35:21 +0200

    Saving the arguments

--- 12.BuildingACommandLineProgram/minigrep/src/main.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -3,8 +3,21 @@
  * Created       : Wed Jul 13 2022
  * Author        : Zolo
  * Github        : https://github.com/zolodev
- * Description   : Building a Command Line Program minigrep
+ * Description   : Building a Command Line Program minigrep, a small program
+ *                 to search files for a specific string similar to the
+ *                 program `grep`
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {}
+use std::env;
+fn main() {
+    // Collect the arguments into a vector
+    let args: Vec<String> = env::args().collect();
+
+    // Saving the arguments
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("in file {}", filename);
+}
