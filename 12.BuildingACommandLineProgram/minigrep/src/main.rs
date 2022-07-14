e2118bbd3712ac8cef3edc64f97c751f9b3164c2
Zolo <zolo@unicornab.se>
2022-07-14 22:28:14 +0200

    Starting to refactor a little

--- 12.BuildingACommandLineProgram/minigrep/src/main.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -17,13 +17,25 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // Saving the arguments
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("in file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
