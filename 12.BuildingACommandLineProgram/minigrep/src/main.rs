858147a58315c93bd1e1c91fef41a588f657a3c2
Zolo <zolo@unicornab.se>
2022-07-14 22:52:02 +0200

    improving error handling

--- 12.BuildingACommandLineProgram/minigrep/src/main.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -11,13 +11,17 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // Collect the arguments into a vector
     let args: Vec<String> = env::args().collect();
 
     // Saving the arguments
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("in file {}", config.filename);
@@ -34,14 +38,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
