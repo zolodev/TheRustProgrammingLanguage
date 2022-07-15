36725c3296c2ed06987473fcbcb0e8d3a0a1e04b
Zolo <zolo@unicornab.se>
2022-07-15 09:58:04 +0200

    Refactoring to logic to lib.rs

---
+++ 12.BuildingACommandLineProgram/minigrep/src/lib.rs
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Filename      : lib.rs
+ * Created       : Fri Jul 15 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Adding the lib.rs according to Rust idiom and best
+ *                 practices. This file will contain the app logic and tests.
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::{error::Error, fs};
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
--- 12.BuildingACommandLineProgram/minigrep/src/main.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -9,10 +9,9 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::env;
-use std::error::Error;
-use std::fs;
-use std::process;
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     // Collect the arguments into a vector
@@ -24,37 +23,13 @@ fn main() {
         process::exit(1);
     });
 
+    // Feedback to the user
     println!("Searching for {}", config.query);
     println!("in file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    // Run the program using the configuration
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
