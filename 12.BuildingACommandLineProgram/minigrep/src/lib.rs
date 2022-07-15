838a849816cf26e2a51513611478dd23e0a729b3
Zolo <zolo@unicornab.se>
2022-07-15 10:29:45 +0200

    adding a stub and a thought

--- 12.BuildingACommandLineProgram/minigrep/src/lib.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/lib.rs
@@ -29,7 +29,34 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+    fs::read_to_string(config.filename)?;
 
     Ok(())
+}
+
+fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    // TODO:
+    // Iterate through each line of the contents.
+    // Check whether the line contains our query string.
+    // If it does, add it to the list of values we’re returning.
+    // If it doesn’t, do nothing.
+    // Return the list of results that match.
+
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
