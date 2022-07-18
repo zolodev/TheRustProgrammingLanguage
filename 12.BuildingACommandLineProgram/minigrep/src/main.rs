722eb5a1dab08dac339d35f8a088f027dc9c6500
Zolo <zolo@unicornab.se>
2022-07-18 09:49:49 +0200

    Updating error message to print to error stream

--- 12.BuildingACommandLineProgram/minigrep/src/main.rs
+++ 12.BuildingACommandLineProgram/minigrep/src/main.rs
@@ -19,13 +19,13 @@ fn main() {
 
     // Saving the arguments
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // Run the program using the configuration
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
