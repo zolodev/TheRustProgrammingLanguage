87ea1820611f1ec6002af589069b2ac3b87e75a1
Zolo <zolo@unicornab.se>
2022-07-21 08:06:31 +0200

    working workspace

---
+++ 14.MoreAboutCargoAndCrates/add/add_one/src/lib.rs
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Filename      : lib.rs
+ * Created       : Thu Jul 21 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 14
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ 14.MoreAboutCargoAndCrates/add/adder/src/main.rs
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Thu Jul 21 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 14
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {
+    let num = 10;
+
+    println!(
+        "Hello, world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+}
