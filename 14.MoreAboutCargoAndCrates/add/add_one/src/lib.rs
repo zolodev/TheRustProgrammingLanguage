f0e875a3d53bb07696f1a9394010c29a4a949e75
Zolo <zolo@unicornab.se>
2022-07-21 15:55:46 +0200

    updating test case

--- 14.MoreAboutCargoAndCrates/add/add_one/src/lib.rs
+++ 14.MoreAboutCargoAndCrates/add/add_one/src/lib.rs
@@ -15,9 +15,10 @@ pub fn add_one(x: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use crate::add_one;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(3, add_one(2));
     }
 }
