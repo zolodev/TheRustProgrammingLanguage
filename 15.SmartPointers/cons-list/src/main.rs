ecbf3f85e834aec27380e1f06111b8b84fa5278d
Zolo <zolo@unicornab.se>
2022-07-25 18:04:24 +0200

    Listing 15-24 Using both Rc<T> and RefCell<T>

--- 15.SmartPointers/cons-list/src/main.rs
+++ 15.SmartPointers/cons-list/src/main.rs
@@ -7,12 +7,13 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::{ops::Deref, rc::Rc};
+use std::{cell::RefCell, ops::Deref, rc::Rc};
 
 use crate::List::{Cons, Nil};
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -43,18 +44,30 @@ fn hello(name: &str) {
 }
 
 fn main() {
-    let _list = Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil))))));
+    let value = Rc::new(RefCell::new(5));
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _list = Cons(
+        Rc::new(RefCell::new(1)),
+        Rc::new(Cons(
+            Rc::new(RefCell::new(2)),
+            Rc::new(Cons(Rc::new(RefCell::new(3)), Rc::new(Nil))),
+        )),
+    );
 
-    let b = Cons(3, Rc::clone(&a)); // Using Rc to clone a reference
-    println!("count after crating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    println!("a before = {:?}", a);
+    println!("b before = {:?}", b);
+    println!("c before = {:?}", c);
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 
     let x = 5;
     let y = MyBox::new(x); // similar to &x, using Box<T> creating an instance
