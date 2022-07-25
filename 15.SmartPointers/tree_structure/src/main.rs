e48967102350647659d561ce407be7f69215179d
Zolo <zolo@unicornab.se>
2022-07-25 22:10:15 +0200

    Listing 15-28 adding weak reference parent

--- 15.SmartPointers/tree_structure/src/main.rs
+++ 15.SmartPointers/tree_structure/src/main.rs
@@ -1,4 +1,4 @@
-/*****************************************************************************
+/**************************************************************&***************
  * Filename      : main.rs
  * Created       : Mon Jul 25 2022
  * Author        : Zolo
@@ -7,21 +7,33 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
-    children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>, // If a parent node is dropped, drop children
+    children: RefCell<Vec<Rc<Node>>>, // if a child is dropped, parent remain
 }
 fn main() {
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
