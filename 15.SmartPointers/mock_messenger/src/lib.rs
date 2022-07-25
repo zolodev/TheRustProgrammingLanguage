21ccab7105d7ed91c500a42d6f2f1f0bddb42041
Zolo <zolo@unicornab.se>
2022-07-25 17:28:51 +0200

    Listing 15-22 Using RefCell<T> with a Mock object

--- 15.SmartPointers/mock_messenger/src/lib.rs
+++ 15.SmartPointers/mock_messenger/src/lib.rs
@@ -12,14 +12,14 @@ pub trait Messenger {
 }
 
 pub struct LimitTracker<'a, T: Messenger> {
-    messenger; &'a T,
+    messenger: &'a T,
     value: usize,
     max: usize,
 }
 
 impl<'a, T> LimitTracker<'a, T>
 where
-T: Messenger,
+    T: Messenger,
 {
     pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
         LimitTracker {
@@ -35,8 +35,7 @@ T: Messenger,
         let percentage_of_max = self.value as f64 / self.max as f64;
 
         if percentage_of_max >= 1.0 {
-            self.messenger
-                .send("Error: You are over you quota!");
+            self.messenger.send("Error: You are over you quota!");
         } else if percentage_of_max >= 0.9 {
             self.messenger
                 .send("Urgent warning: You've used up over 90% of you quota!");
@@ -47,12 +46,38 @@ T: Messenger,
     }
 }
 
-
 #[cfg(test)]
 mod tests {
+
+    use std::cell::RefCell;
+
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
