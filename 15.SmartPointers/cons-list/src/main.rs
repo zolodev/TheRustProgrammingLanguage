15e41f5e7a8e22fa7e4cccb46a469f3c544d970b
Zolo <zolo@unicornab.se>
2022-07-25 21:41:18 +0200

    Listing 15-26 Overflow the stack cycle

--- 15.SmartPointers/cons-list/src/main.rs
+++ 15.SmartPointers/cons-list/src/main.rs
@@ -7,91 +7,45 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::{cell::RefCell, ops::Deref, rc::Rc};
+use std::{cell::RefCell, rc::Rc};
 
 use crate::List::{Cons, Nil};
 
 #[derive(Debug)]
 enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
     }
-}
-
-// Necessary in order for the assert_eq!(5, *y) to compile.
-// Missing Deref due to *y.
-// Similar to how Box<T> from the standard library works.
-impl<T> Deref for MyBox<T> {
-    // syntax defines an associated type for Deref
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        // Returns a reference to the value we want to access with the *
-        // and returns the first value in a tuple by calling .0 on &self
-        &self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    let _list = Cons(
-        Rc::new(RefCell::new(1)),
-        Rc::new(Cons(
-            Rc::new(RefCell::new(2)),
-            Rc::new(Cons(Rc::new(RefCell::new(3)), Rc::new(Nil))),
-        )),
-    );
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
 
-    println!("a before = {:?}", a);
-    println!("b before = {:?}", b);
-    println!("c before = {:?}", c);
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
 
-    *value.borrow_mut() += 10;
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
-
-    let x = 5;
-    let y = MyBox::new(x); // similar to &x, using Box<T> creating an instance
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y); // dereference operator to follow the pointer
-
-    // The difference between &x and Box::new(x) is that
-    // we set y to be an instance of a box pointing to a copied value of x
-    // rather than a reference pointing to the value of x.
-
-    // *y is the same as *(y.deref()) behind the scene
-    assert_eq!(5, *(y.deref()));
-
-    let m = MyBox::new(String::from("Rust"));
-
-    // This works because of the deref coercion
-    hello(&m);
-
-    // If we would not had the deref coercion the code would look like:
-    hello(&(*m));
-
-    // or ...
-    hello(&(*m)[..]);
-
-    // There is no runtime penalty for taking advantages of deref coercion!
+    // Uncomment the next line to see the cycle
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
