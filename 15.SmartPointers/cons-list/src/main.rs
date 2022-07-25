74a1cb13bbca5160514cbe05a76d1d83b9f33eb5
Zolo <zolo@unicornab.se>
2022-07-25 10:46:15 +0200

    lsiting 15-6 assert a point to a reference

--- 15.SmartPointers/cons-list/src/main.rs
+++ 15.SmartPointers/cons-list/src/main.rs
@@ -15,5 +15,11 @@ enum List {
 }
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
