f4c77f69b18b894f835b7ee1e06cfdf38e922e53
Zolo <zolo@unicornab.se>
2022-07-25 10:26:35 +0200

    fixing the code according to listing 15-5

--- 15.SmartPointers/cons-list/src/main.rs
+++ 15.SmartPointers/cons-list/src/main.rs
@@ -10,10 +10,10 @@
 use crate::List::{Cons, Nil};
 
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
 fn main() {
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
