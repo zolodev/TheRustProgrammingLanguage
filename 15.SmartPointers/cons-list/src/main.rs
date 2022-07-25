01149e1fd6afc59d2f37859b511200ad57e89349
Zolo <zolo@unicornab.se>
2022-07-25 11:29:48 +0200

    Listing 15-10 explaining the deref

--- 15.SmartPointers/cons-list/src/main.rs
+++ 15.SmartPointers/cons-list/src/main.rs
@@ -7,6 +7,8 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::ops::Deref;
+
 use crate::List::{Cons, Nil};
 
 enum List {
@@ -14,11 +16,33 @@ enum List {
     Nil,
 }
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+// Necessary in order for the assert_eq!(5, *y) to compile.
+// Missing Deref due to *y.
+// Similar to how Box<T> from the standard library works.
+impl<T> Deref for MyBox<T> {
+    // syntax defines an associated type for Deref
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // Returns a reference to the value we want to access with the *
+        // and returns the first value in a tuple by calling .0 on &self
+        &self.0
+    }
+}
+
 fn main() {
     let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let x = 5;
-    let y = Box::new(x); // similar to &x, using Box<T> creating an instance
+    let y = MyBox::new(x); // similar to &x, using Box<T> creating an instance
 
     assert_eq!(5, x);
     assert_eq!(5, *y); // dereference operator to follow the pointer
@@ -26,4 +50,7 @@ fn main() {
     // The difference between &x and Box::new(x) is that
     // we set y to be an instance of a box pointing to a copied value of x
     // rather than a reference pointing to the value of x.
+
+    // *y is the same as *(y.deref()) behind the scene
+    assert_eq!(5, *(y.deref()));
 }
