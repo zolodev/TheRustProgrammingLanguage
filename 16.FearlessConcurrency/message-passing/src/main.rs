7c957468b8720e5b2ce2093d6f1a59ce9cb11ff0
Zolo <zolo@unicornab.se>
2022-07-26 12:20:28 +0200

    Listing 16-9 Channels

---
+++ 16.FearlessConcurrency/message-passing/src/main.rs
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Tue Jul 26 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 16
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::{sync::mpsc, thread};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+
+        tx.send(val).unwrap();
+        // println!("val is {}", val);
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
