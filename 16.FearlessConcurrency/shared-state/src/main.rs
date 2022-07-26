409eb0f82024fc358d487a29456ae0eba07fa6f5
Zolo <zolo@unicornab.se>
2022-07-26 19:31:10 +0200

    Listing 16-15 Using Arc

--- 16.FearlessConcurrency/shared-state/src/main.rs
+++ 16.FearlessConcurrency/shared-state/src/main.rs
@@ -1,4 +1,4 @@
-/*****************************************************************************
+/**************************************************************&***************
  * Filename      : main.rs
  * Created       : Tue Jul 26 2022
  * Author        : Zolo
@@ -7,7 +7,11 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 fn main() {
     let m = Mutex::new(5);
@@ -18,4 +22,28 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+
+    // Using Arc instead of Rc to make it atomic (thread safe)
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        // using Arc instead of Rc
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+            thread::sleep(Duration::from_secs(1));
+        });
+        handles.push(handle);
+    }
+
+    let mut count = 0;
+    for handle in handles {
+        handle.join().unwrap();
+        count += 1;
+        println!("joing thread {}", count);
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
