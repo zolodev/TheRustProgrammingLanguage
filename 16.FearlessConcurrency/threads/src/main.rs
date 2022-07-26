c7e60d0f72221737c15208133aa1061a5fbdb840
Zolo <zolo@unicornab.se>
2022-07-26 08:46:27 +0200

    Init chapter 16

---
+++ 16.FearlessConcurrency/threads/src/main.rs
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Tue Jul 26 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 16
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {}
