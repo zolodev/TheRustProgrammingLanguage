8c96f99534e325f2d427797c5f28e0af2b5c1f13
Zolo <zolo@unicornab.se>
2022-07-26 18:57:50 +0200

    Listing 16-11 multiple transmitters one receiver

--- 16.FearlessConcurrency/message-passing/src/main.rs
+++ 16.FearlessConcurrency/message-passing/src/main.rs
@@ -12,6 +12,8 @@ use std::{sync::mpsc, thread, time::Duration};
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
+
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -22,6 +24,20 @@ fn main() {
 
         for val in vals {
             tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
