ccffac8b0d6cd3b2df083f7a0781e4665cf20bdc
Zolo <zolo@unicornab.se>
2022-07-26 10:52:01 +0200

    Listing 16-1 spawning a new thread

--- 16.FearlessConcurrency/threads/src/main.rs
+++ 16.FearlessConcurrency/threads/src/main.rs
@@ -7,4 +7,18 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {}
+use std::{thread, time::Duration};
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
