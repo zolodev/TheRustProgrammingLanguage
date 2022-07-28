238e0a39ab3be2a47e7846aa4628b48711a3324a
Zolo <zolo@unicornab.se>
2022-07-28 09:56:44 +0200

    Adding code for Listing 17-3

---
+++ 17.ObjectOrientedProgramming/gui/src/lib.rs
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Filename      : lib.rs
+ * Created       : Thu Jul 28 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 17
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
