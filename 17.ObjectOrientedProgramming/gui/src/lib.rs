9b5e3ee53e7ad22b05e9c5dbfe5f0ed64f4777d0
Zolo <zolo@unicornab.se>
2022-07-28 10:03:38 +0200

    Revert "Adding code for Listing 17-4"
    This reverts commit fb9bcc1f7db01f376e9f833ce5be470803c2f3e1.

--- 17.ObjectOrientedProgramming/gui/src/lib.rs
+++ 17.ObjectOrientedProgramming/gui/src/lib.rs
@@ -7,22 +7,12 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use std::path::Component;
-
 pub trait Draw {
     fn draw(&self);
 }
 
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
-}
-
-impl Screen {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
-    }
 }
 
 #[cfg(test)]
