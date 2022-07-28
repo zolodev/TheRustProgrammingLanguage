ee5fc4b685467dac908ca93327a83f84c2f7c4c5
Zolo <zolo@unicornab.se>
2022-07-28 16:55:04 +0200

    Adding code for Listing 18-1 Mixing if and if let

--- 18.PatternsAndMatching/patterns/src/main.rs
+++ 18.PatternsAndMatching/patterns/src/main.rs
@@ -7,4 +7,22 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-fn main() {}
+fn main() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using pruple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
