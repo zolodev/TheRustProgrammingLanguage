e62f084439adfcc2815665a20dfdd530bd0681ca
Zolo <zolo@unicornab.se>
2022-07-28 22:07:47 +0200

    Adding code for Listing 18-27

--- 18.PatternsAndMatching/patterns/src/main.rs
+++ 18.PatternsAndMatching/patterns/src/main.rs
@@ -227,6 +227,15 @@ fn main() {
         Some(x) => println!("The number {} is odd", x),
         None => (),
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
 }
 
 struct Point {
