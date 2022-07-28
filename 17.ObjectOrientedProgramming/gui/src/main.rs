5123004055c37e5a23edf705f7d47d1aaf847e9f
Zolo <zolo@unicornab.se>
2022-07-28 10:18:27 +0200

    Adding code Listing 17-8

---
+++ 17.ObjectOrientedProgramming/gui/src/main.rs
@@ -0,0 +1,25 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Thu Jul 28 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 17
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+use gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        todo!();
+    }
+}
+
+fn main() {}
