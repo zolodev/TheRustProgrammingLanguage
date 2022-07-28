4afba1ef835ea8137fb0a0a9550233b74989654a
Zolo <zolo@unicornab.se>
2022-07-28 10:03:59 +0200

    Adding code for Listing 17-5

--- 17.ObjectOrientedProgramming/gui/src/lib.rs
+++ 17.ObjectOrientedProgramming/gui/src/lib.rs
@@ -7,12 +7,22 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::path::Component;
+
 pub trait Draw {
     fn draw(&self);
 }
 
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
 }
 
 #[cfg(test)]
