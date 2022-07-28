daa2f3001f404573421a743f5dcc3495ed85b10f
Zolo <zolo@unicornab.se>
2022-07-28 21:59:58 +0200

    Adding code for Listings 18-26 match with guard

--- 18.PatternsAndMatching/patterns/src/main.rs
+++ 18.PatternsAndMatching/patterns/src/main.rs
@@ -219,6 +219,14 @@ fn main() {
             println!("Some numbers: {}, {}", first, last);
         }
     }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
 }
 
 struct Point {
