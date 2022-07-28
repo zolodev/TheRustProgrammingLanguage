eb37b7cb6cf332e18c97d98951bba64c48b17a45
Zolo <zolo@unicornab.se>
2022-07-28 10:25:13 +0200

    Adding code for listing 17-9

--- 17.ObjectOrientedProgramming/gui/src/lib.rs
+++ 17.ObjectOrientedProgramming/gui/src/lib.rs
@@ -11,34 +11,36 @@ pub trait Draw {
     fn draw(&self);
 }
 
-// pub struct Screen {
-//     pub components: Vec<Box<dyn Draw>>,
-// }
-
-// impl Screen {
-//     pub fn run(&self) {
-//         for component in self.components.iter() {
-//             component.draw();
-//         }
-//     }
-// }
-
-// Alternative implementation of Screen using generic type and traid bound
-
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
 }
 
-impl<T> Screen<T>
-where
-    T: Draw,
-{
+impl Screen {
     pub fn run(&self) {
         for component in self.components.iter() {
             component.draw();
         }
     }
 }
+
+// Alternative implementation of Screen using generic type and traid bound
+// This is limited to only hold ONE type.
+// For example a vector only containing multiple Buttons OR something else.
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
 
 pub struct Button {
     pub width: u32,
--- 17.ObjectOrientedProgramming/gui/src/main.rs
+++ 17.ObjectOrientedProgramming/gui/src/main.rs
@@ -7,7 +7,7 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
-use gui::Draw;
+use gui::{Button, Draw, Screen};
 
 struct SelectBox {
     width: u32,
@@ -22,4 +22,25 @@ impl Draw for SelectBox {
     }
 }
 
-fn main() {}
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
