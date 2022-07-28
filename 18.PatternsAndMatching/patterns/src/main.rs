eff4b01715c7686c5c6aec544082abd45946ad99
Zolo <zolo@unicornab.se>
2022-07-28 20:20:25 +0200

    Adding code for Listing 18-11 match with shadow

--- 18.PatternsAndMatching/patterns/src/main.rs
+++ 18.PatternsAndMatching/patterns/src/main.rs
@@ -83,6 +83,17 @@ fn main() {
     if let x = 5 {
         println!("{}", x);
     }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
 
 // x is another pattern in a function like the one below.
