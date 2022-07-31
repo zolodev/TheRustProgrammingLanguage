92022bfdd1a16252b03c4be5720490afe7d8fba9
Zolo <zolo@unicornab.se>
2022-07-31 19:29:18 +0200

    Adding code for enum initializer

--- 19.AdvancedFeatures/functions-example/src/main.rs
+++ 19.AdvancedFeatures/functions-example/src/main.rs
@@ -15,8 +15,30 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+#[derive(Debug)]
+enum Status {
+    Value(u32),
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    println!("list of strings: {:?}", list_of_numbers);
+    println!("list of strings: {:?}", list_of_strings);
+
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+
+    println!("list of strings: {:?}", list_of_strings);
+
+    // Quick fill a Vector using a enum initializer
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+    for status in list_of_statuses {
+        println!("status: {:?}", status);
+    }
 }
