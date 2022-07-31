3b88f45bf79d27546937aa3264e4494c6af2282d
Zolo <zolo@unicornab.se>
2022-07-31 11:09:35 +0200

    Adding Listing 19-6 with some modifications

--- 19.AdvancedFeatures/unsafe-example/src/main.rs
+++ 19.AdvancedFeatures/unsafe-example/src/main.rs
@@ -7,6 +7,8 @@
 *****************************************************************************/
 #![warn(clippy::all, clippy::pedantic)]
 
+use core::slice;
+
 fn main() {
     let mut num = 5;
 
@@ -31,6 +33,34 @@ fn main() {
         // either another unsafe function or an unsafe block like this.
         dangerous();
     }
+
+    // split_at_mut() runs unsafe code to perform this action
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+
+    // another custom implementation of split_at_mut
+    let (left, right) = my_split_at_mut(r, 2);
+    assert_eq!(left, &mut [1, 2]);
+    assert_eq!(right, &mut [3, 4, 5, 6]);
 }
 
 unsafe fn dangerous() {}
+
+fn my_split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
