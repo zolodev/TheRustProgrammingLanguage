622f028369190e6b30c18fe3fa2bbd2b1313e7a1
Zolo <zolo@unicornab.se>
2022-07-31 11:50:23 +0200

    Adding code for Listing 19-10

--- 19.AdvancedFeatures/global_variables/src/main.rs
+++ 19.AdvancedFeatures/global_variables/src/main.rs
@@ -8,7 +8,20 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 static HELLO_WORLD: &str = "Hello World!";
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
 
 fn main() {
     println!("name is: {}", HELLO_WORLD);
+
+    add_to_count(3);
+
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
 }
