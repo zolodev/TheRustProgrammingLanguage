cec43322d96fa171659f244d8dbfd991b275e7f7
Zolo <zolo@unicornab.se>
2022-07-31 10:41:37 +0200

    Adding code for Listings 19-1 to 19-3

--- 19.AdvancedFeatures/unsafe-example/src/main.rs
+++ 19.AdvancedFeatures/unsafe-example/src/main.rs
@@ -1,4 +1,4 @@
-/*****************************************************************************
+/**************************************************************&***************
  * Filename      : main.rs
  * Created       : Sun Jul 31 2022
  * Author        : Zolo
@@ -14,4 +14,15 @@ fn main() {
     let _r2 = &mut num as *mut i32; // Mutable raw pointer to num
 
     println!("immutable ptr: {:?}, mutable ptr: {:?}", _r1, _r2);
+
+    let _address = 0x012345usize;
+    let _r3 = _address as *const i32;
+    println!("unknown value ptr: {:?}", _r3);
+
+    unsafe {
+        println!("_r1 is {}", *_r1); // _r1 is 5
+        println!("_r2 is {}", *_r2); // _r2 is 5
+
+        // println!("_r3 is {}", *_r3); // Error, segmentation fault
+    }
 }
