7dbc8b8f719111a9dfcff69ad3c7394ef2bbc849
Zolo <zolo@unicornab.se>
2022-07-31 10:18:39 +0200

    init chapter 19

---
+++ 19.AdvancedFeatures/unsafe-example/src/main.rs
@@ -0,0 +1,10 @@
+/*****************************************************************************
+ * Filename      : main.rs
+ * Created       : Sun Jul 31 2022
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   : Working through the Rust book chapter 19
+*****************************************************************************/
+#![warn(clippy::all, clippy::pedantic)]
+
+fn main() {}
