f2ae5f006621021e747fca56da2610d432a745a7
Zolo <zolo@unicornab.se>
2022-07-31 11:43:46 +0200

    Creating an interface to C

--- 19.AdvancedFeatures/ffi/src/main.rs
+++ 19.AdvancedFeatures/ffi/src/main.rs
@@ -11,8 +11,14 @@
 //!
 //! This is a demonstration of how to interact with an external C module.
 
+// Accessing C library and the function abs()
 extern "C" {
     fn abs(input: i32) -> i32;
+}
+
+// Creating an interface that can be called from C
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
 }
 
 fn main() {
