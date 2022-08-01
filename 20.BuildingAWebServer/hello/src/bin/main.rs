92847e58f0f8c8775e87956e6f1aba1d0a73295c
Zolo <zolo@unicornab.se>
2022-08-01 17:57:14 +0200

    bug fix

--- 20.BuildingAWebServer/hello/src/bin/main.rs
+++ 20.BuildingAWebServer/hello/src/bin/main.rs
@@ -17,10 +17,10 @@ use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
 
         pool.execute(|| {
             handle_connection(_stream);
